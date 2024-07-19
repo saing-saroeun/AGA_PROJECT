28ead2779d5971f9fbf43e1a6116dcc3ab136582
GURUTECH <saingsaroeuninfo@gmail.com>
2024-07-19 18:51:33 +0700

    UserController was create

---
+++ AGA_GitProject/Controllers/UserController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AGA_GitProject.Controllers
+{
+    public class UserController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
