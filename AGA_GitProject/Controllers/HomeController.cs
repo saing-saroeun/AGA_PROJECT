b40582b01cb3373d68690fa8b438906083be48cd
GURUTECH <GURUTECH@DESKTOP-9B5UCFT>
2024-07-19 18:31:18 +0700

    Add project files.

---
+++ AGA_GitProject/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using AGA_GitProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AGA_GitProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AGA_GitProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AGA_GitProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
