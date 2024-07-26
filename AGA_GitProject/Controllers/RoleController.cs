5ddcebddcefb2cc3b83995ff382f39e58ac7ee2b
Sky007 <sophealeakhena.yorn168@gmail.com>
2024-07-26 18:42:07 +0700

    RoleController has been create by leakhena

---
+++ AGA_GitProject/Controllers/RoleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AGA_GitProject.Controllers
+{
+    public class RoleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
